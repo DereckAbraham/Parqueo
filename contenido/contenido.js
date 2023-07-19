692d61a23dbdb43a66d7d84a8454c839afddb537
DereckAbraham <126488825+DereckAbraham@users.noreply.github.com>
2023-07-19 10:16:54 -0600

    Add files via upload

---
+++ contenido/contenido.js
@@ -0,0 +1,5 @@
+let contenido_del_DOM =  document.querySelector(".contenido");
+
+let contenido = `<p> Seccion del Contenido </p>`
+
+contenido_del_DOM.innerHTML = contenido;
--- js/footer.js
+++ js/footer.js
@@ -1,5 +1,12 @@
 let footer_del_DOM =  document.querySelector(".footer");
 
-let footer = `<p> Seccion del Footer </p>`
+let footer = `
+    <div class="bajo">
+        <span>@dacarranza</span>
+        <a href="https://github.com/DereckAbraham?tab=repositories">
+        <img  class="github" src="./img/github.png">
+        </a>
+    </div>
+`
 
 footer_del_DOM.innerHTML = footer;
