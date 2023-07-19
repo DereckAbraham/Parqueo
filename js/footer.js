8bf56a49fa2549e0ef728986a14a6911689edc57
DereckAbraham <126488825+DereckAbraham@users.noreply.github.com>
2023-07-19 08:32:15 -0600

    Add files via upload

---
+++ js/footer.js
@@ -0,0 +1,5 @@
+let footer_del_DOM =  document.querySelector(".footer");
+
+let footer = `<p> Seccion del Footer </p>`
+
+footer_del_DOM.innerHTML = footer;
---
+++ js/header.js
@@ -0,0 +1,24 @@
+//Localizar el nodo en el DOM
+//nodo:  Etiqueta en su archivo .html
+
+let header_del_DOM = document.querySelector(".header");
+
+//Almacenar en una variable el contenido a agregar
+let header = `
+<div class = "contenedor">
+<img src="./img/park.png">
+<p>Titulo del Header</p>
+<nav>
+    <ul class ="tipos">
+        <li>Transporte Pesado</li>
+        <li>SUV</li>
+        <li>Sedanes</li>
+        <li>Motocicletas</li>
+        <li>Otros</li>
+    </ul>
+</nav>
+ </div>
+`;
+
+//Agregar el contenido a la referecia del DOM
+header_del_DOM.innerHTML = header;
